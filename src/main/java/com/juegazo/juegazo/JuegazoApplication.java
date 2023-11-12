f8d4d1687bfa72e259558d65a764bcdeba01219b
Juan Quintero <juan.quintero@asopagos.com>
2023-11-12 18:16:53 -0500

    a

---
+++ src/main/java/com/juegazo/juegazo/JuegazoApplication.java
@@ -0,0 +1,13 @@
+package com.juegazo.juegazo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegazoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuegazoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juegazo/juegazo/JuegazoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juegazo.juegazo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegazoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
